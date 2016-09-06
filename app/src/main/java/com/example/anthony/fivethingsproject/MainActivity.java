684c0c087339905f4769e1053c3dc29ad80919f2
Anthony <b2021590@gmail.com>
2016-09-06 17:11:00 +0800

    Click on first button will jump to another page

--- app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
+++ app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.anthony.fivethingsproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button01 = (Button)findViewById(R.id.atari_breakout);
+
+        button01.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v) {
+
+                // TODO Auto-generated method stub
+
+                toAtariBreakout();
+
+            }
+
+        });
+    }
+
+    private void toAtariBreakout(){
+        setContentView(R.layout.atari_breakout);
     }
 }
