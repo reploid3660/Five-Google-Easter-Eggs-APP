6375379702fcd825d0df77d7fa7f2d2ce68faa01
Anthony <b2021590@gmail.com>
2016-09-07 15:43:59 +0800

    Smartphone back button works for first button -when click on first button, it will enter a page -in this page, click on back button, and it will return to the main page

--- app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
+++ app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anthony.fivethingsproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,21 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button01 = (Button)findViewById(R.id.atari_breakout);
-
         button01.setOnClickListener(new Button.OnClickListener(){
 
             public void onClick(View v) {
-
-                // TODO Auto-generated method stub
-
-                toAtariBreakout();
-
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
             }
-
         });
     }
-
+    /*
+        **Lead to the Atari Breakout page
+     */
     private void toAtariBreakout(){
         setContentView(R.layout.atari_breakout);
     }
+
 }
