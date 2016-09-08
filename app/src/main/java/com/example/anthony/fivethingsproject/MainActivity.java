e1fa248c035c8ebe0d65944b3d8b4e3094ec3090
Anthony <b2021590@gmail.com>
2016-09-08 21:32:54 +0800

    every button isolated from onclick method

--- app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
+++ app/src/main/java/com/example/anthony/fivethingsproject/MainActivity.java
@@ -8,14 +8,15 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button01;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Recursion button
-        Button button03 = (Button)findViewById(R.id.recursion_button);
-        button03.setOnClickListener(new Button.OnClickListener(){
+        Button button03 = (Button) findViewById(R.id.recursion_button);
+        button03.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Recursion.class);
@@ -23,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //Tic Tac Toe button
-        Button button04 = (Button)findViewById(R.id.tic_tac_toe_button);
-        button04.setOnClickListener(new Button.OnClickListener(){
+        Button button04 = (Button) findViewById(R.id.tic_tac_toe_button);
+        button04.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, TicTacToe.class);
@@ -32,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         //Flip!Google! button
-        Button button05 = (Button)findViewById(R.id.flip_google_button);
-        button05.setOnClickListener(new Button.OnClickListener(){
+        Button button05 = (Button) findViewById(R.id.flip_google_button);
+        button05.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, FlipGoogle.class);
@@ -41,15 +42,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void atariBreakoutButton(View view){
-        //Atari Breakout button
+
+    /*
+        **Atari Breakout button
+     */
+    public void atariBreakoutButton(View view) {
         Intent intent = new Intent(MainActivity.this, AtariBreakout.class);
         startActivity(intent);
     }
 
-    public void doABarrelRollButton(View view){
-        //Do a barrel roll button
+    /*
+       **Do a barrel roll button button
+    */
+    public void doABarrelRollButton(View view) {
         Intent intent = new Intent(MainActivity.this, DoABarrelRoll.class);
+        startActivity(intent);
+    }
+
+    /*
+       **Recursion button
+    */
+    public void recursionButton(View view) {
+        Intent intent = new Intent(MainActivity.this, Recursion.class);
+        startActivity(intent);
+    }
+
+    /*
+       **Tic Tac Toe button
+    */
+    public void ticTacToeButton(View view) {
+        Intent intent = new Intent(MainActivity.this, TicTacToe.class);
+        startActivity(intent);
+    }
+
+    /*
+       **Flip!Google! button
+    */
+    public void flipGoogleButton(View view) {
+        Intent intent = new Intent(MainActivity.this, FlipGoogle.class);
         startActivity(intent);
     }
 }
